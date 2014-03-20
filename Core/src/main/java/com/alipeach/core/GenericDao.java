6d94bae118aa29d557fb92f562a0f8818839d5d2
chistianchen <328427972@qq.com>
2014-03-21 00:00:54 +0800

    removed spring-orm

--- Core/src/main/java/com/alipeach/core/GenericDao.java
+++ Core/src/main/java/com/alipeach/core/GenericDao.java
@@ -1,19 +1,22 @@
+package com.alipeach.core;
+
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * @author Chen Haoming
  */
-public interface GenericDao<T, PK> {
+public interface GenericDao<T, PK extends Serializable> {
 
     T save (T t);
 
     T get (PK pk);
 
-    boolean delete (PK pk);
+    void delete (T t);
 
     List<T> save (List<T> list);
 
-    int delete (List<T> list);
+    void delete (List<T> list);
 
     List<T> find (T t);
 
---
+++ Core/src/main/java/com/alipeach/core/hibernate/HibernateGenericDao.java
@@ -0,0 +1,70 @@
+package com.alipeach.core.hibernate;
+
+import com.alipeach.core.GenericDao;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author Chen Haoming
+ */
+public class HibernateGenericDao<T, PK extends Serializable> implements GenericDao<T, PK> {
+
+    private Class<T> persistentClass;
+
+    private SessionFactory sessionFactory;
+
+    public HibernateGenericDao (Class<T> persistentClass, SessionFactory sessionFactory) {
+        this.persistentClass = persistentClass;
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public T save (T t) {
+        sessionFactory.getCurrentSession ().save (t);
+        return t;
+    }
+
+    @SuppressWarnings ("unchecked")
+    @Override
+    public T get (PK pk) {
+        return (T) sessionFactory.getCurrentSession ().get (persistentClass, pk);
+    }
+
+    @Override
+    public void delete (T t) {
+        sessionFactory.getCurrentSession ().delete (t);
+    }
+
+    @Override
+    public List<T> save (List<T> list) {
+        Session session = sessionFactory.getCurrentSession ();
+        for (T t : list) {
+            session.save (t);
+        }
+        return list;
+    }
+
+    @Override
+    public void delete (List<T> list) {
+        Session session = sessionFactory.getCurrentSession ();
+        for(T t : list) {
+            session.delete (t);
+        }
+    }
+
+    @SuppressWarnings ("unchecked")
+    @Override
+    public List<T> find (T t) {
+        return sessionFactory.getCurrentSession ().createCriteria (persistentClass).add (Example.create (t)).list ();
+    }
+
+    @Override
+    public Object executeSQL (String sql) {
+        throw new UnsupportedOperationException ();
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
