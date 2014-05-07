497b310a18272911f928ce67da072c691f5825a7
yanziffei <512197364@qq.com>
2014-05-07 23:16:03 +0800

    Added Columns to User.

--- UserManager/src/main/java/com/alipeach/usermanager/model/User.java
+++ UserManager/src/main/java/com/alipeach/usermanager/model/User.java
@@ -3,19 +3,28 @@ package com.alipeach.usermanager.model;
 import com.alipeach.core.model.BaseEntity;
 import com.sun.org.glassfish.external.statistics.Stats;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.UUID;
 
 /**
  * @author wjy
  */
+@Entity (name = "t_user")
 public class User extends BaseEntity {
 
+    @Id
+    @Column (name = "uuid", length = 36)
     private String uuid = UUID.randomUUID ().toString ();
 
+    @Column (name = "version", nullable = false)
     private int version = 0;
 
+    @Column (name = "nickname", unique = true, length = 60, nullable = false)
     private String nickName;
 
+    @Column (name = "password", length = 32, nullable = false)
     private String password;
 
     private Status status;
@@ -40,6 +49,7 @@ public class User extends BaseEntity {
         return status.getCode ();
     }
 
+    @Column (name = "status", nullable = false)
     public void setStatus (int code) {
         this.status = Status.getStatus (code);
     }
